8b4740a09a247173a2d195879ffef3f5e6e55643
ccran <1295268266@qq.com>
2018-07-30 23:08:03 +0800

    1-bit and 2-bit Characters

---
+++ src/leetcode/Array/Max_Area_of_Island.java
@@ -0,0 +1,91 @@
+package leetcode.Array;
+
+/**
+ * Given a non-empty 2D array grid of 0's and 1's,
+ * an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+ * You may assume all four edges of the grid are surrounded by water.
+ * Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+ * 找到二维数组中水平垂直方向连续1的和的最大值,dfs问题
+ * <p>
+ * Example 1:
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+ * <p>
+ * Example 2:
+ * [[0,0,0,0,0,0,0,0]]
+ * Given the above grid, return 0.
+ */
+public class Max_Area_of_Island {
+    static class Solution {
+        public int maxAreaOfIsland(int[][] grid) {
+            int max = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[i].length; j++) {
+                    if (grid[i][j] == 1) {
+                        int tmp = 0;
+                        tmp = deepSearch(grid, i, j, tmp);
+                        if (tmp > max)
+                            max = tmp;
+                    }
+                }
+            }
+            return max;
+        }
+
+        public int deepSearch(int[][] grid, int i, int j, int max) {
+            if (grid[i][j] == 1) {
+                grid[i][j] = 0;
+                max++;
+                if (i + 1 < grid.length) {
+                    max = deepSearch(grid, i + 1, j, max);
+                }
+                if (i - 1 >= 0) {
+                    max = deepSearch(grid, i - 1, j, max);
+                }
+                if (j + 1 < grid[i].length) {
+                    max = deepSearch(grid, i, j + 1, max);
+                }
+                if (j - 1 >= 0) {
+                    max = deepSearch(grid, i, j - 1, max);
+                }
+            }
+            return max;
+        }
+
+        public int maxAreaOfIsland_Most_Votes(int[][] grid) {
+            int max_area = 0;
+            for(int i = 0; i < grid.length; i++)
+                for(int j = 0; j < grid[0].length; j++)
+                    if(grid[i][j] == 1)max_area = Math.max(max_area, AreaOfIsland(grid, i, j));
+            return max_area;
+        }
+
+        public int AreaOfIsland(int[][] grid, int i, int j){
+            if( i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1){
+                grid[i][j] = 0;
+                return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
+            }
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        int[][] grid1 = {{0, 1}, {1, 1}};
+        System.out.println(new Solution().maxAreaOfIsland(grid1));
+    }
+}
---
+++ src/leetcode/Array/Move_Zeroes.java
@@ -0,0 +1,62 @@
+package leetcode.Array;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Note:
+ * <p>
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * <p>
+ * 在不改变原有数字顺序的前提下将0全部移动到最后
+ */
+public class Move_Zeroes {
+    static class Solution {
+        public void moveZeroes(int[] nums) {
+            LinkedList<Integer> empty = new LinkedList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) {
+                    empty.add(i);
+                } else {
+                    if (!empty.isEmpty()) {
+                        nums[empty.poll()] = nums[i];
+                        empty.add(i);
+                    }
+                }
+            }
+            while (!empty.isEmpty()) {
+                nums[empty.poll()] = 0;
+            }
+        }
+
+        //对于插入的位置无需存入队列中
+        public void moveZeros_Most_Votes(int[] nums){
+            if (nums == null || nums.length == 0) return;
+
+            int insertPos = 0;
+            for (int num: nums) {
+                if (num != 0) nums[insertPos++] = num;
+            }
+
+            while (insertPos < nums.length) {
+                nums[insertPos++] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        Solution solution = new Solution();
+        solution.moveZeroes(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
