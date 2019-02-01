ee0df774ff820009023f6937d143ef6bcff6993e
ccran <1295268266@qq.com>
2019-02-01 22:11:57 +0800

    2019-02-01

--- src/leetcode/Array/Non_decreasing_Array.java
+++ src/leetcode/Array/Non_decreasing_Array.java
@@ -31,5 +31,16 @@ public class Non_decreasing_Array {
             }
             return true;
         }
+
+        public boolean func(int[] nums){
+            int cnt = 0;
+            for(int i = 1; i < nums.length && cnt<=1 ; i++){
+                if(nums[i-1] > nums[i]){
+                    cnt++;
+                    if(i-2>=0 && nums[i-2] > nums[i]) nums[i] = nums[i-1];
+                }
+            }
+            return cnt<=1;
+        }
     }
 }
