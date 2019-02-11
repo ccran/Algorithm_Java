eb7f8770cc9bb307c2f83d66ed52cb2560c08ece
ccran <1295268266@qq.com>
2019-02-11 23:28:14 +0800

    '2019-02-11'

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] res = "hello world".split(" ");
+		String[] res = "hello.world".split("\\.");
 		Set<String> wordSet = new HashSet<>(Arrays.asList("hello hello world".split(" ")));
 		System.out.println(res);
 	}
