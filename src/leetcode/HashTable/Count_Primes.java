129e706c16da7ffa92b097b439380c0f6080bd0f
ccran <1295268266@qq.com>
2019-04-24 23:40:54 +0800

    2019-04-24

---
+++ src/leetcode/HashTable/Count_Primes.java
@@ -0,0 +1,33 @@
+package leetcode.HashTable;
+
+/**
+ * Count the number of prime numbers less than a non-negative number, n.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ */
+public class Count_Primes {
+    class Solution {
+        public boolean isPrime(int num) {
+            int tmp = (int) Math.sqrt(num);
+            for (int i = 2; i <= tmp; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int countPrimes(int n) {
+            int cnt = 0;
+            for (int i = 2; i < n; i++) {
+                if (isPrime(i))
+                    cnt++;
+            }
+            return cnt;
+        }
+    }
+}
